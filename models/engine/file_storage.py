82608951d27f68325fdbf21c89180616fddba502
wughangar <wughangar.rose@gmail.com>
2023-08-22 13:01:37 +0300

    corrected all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is not None:
+        if cls is None:
             return FileStorage.__objects
         else:
             cls_objs = {}
