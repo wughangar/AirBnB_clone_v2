b989e3e5eef5bb9529804fff43ae86745bd6bb2a
wughangar <wughangar.rose@gmail.com>
2023-09-07 14:37:23 +0000

    file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,16 @@
-import os
-import tarfile
-from datetime import datetime
 from fabric import task
+from datetime import datetime
+from fabric.exceptions import CommandFailed
 
 
 @task
 def do_pack(c):
-    """Generate a .tgz archive from the web_static folder."""
-
-    web_static_dir = '/root/AirBnB_clone/web_static'
-    # Ensure the 'versions' directory exists
-    versions_dir = os.path.join(web_static_dir, 'versions')
-    if not os.path.exists(versions_dir):
-        os.makedirs(versions_dir)
-
-    # Generate a unique archive name based on the current date and time
-    now = datetime.utcnow()
-    formatted_time = now.strftime("%Y%m%d%H%M%S")
-    archive_name = f"web_static_{formatted_time}.tgz"
-
-    # Create the .tgz archive with verbose output
     try:
-        with c.prefix("cd " + web_static_dir):
-            c.local(f"tar -cvzf {os.path.join(versions_dir, archive_name)} .")
-        return os.path.abspath(os.path.join(versions_dir, archive_name))
-    except Exception as e:
-        print(f"Error creating archive: {str(e)}")
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = "web_static_" + current_time + ".tgz"
+        c.local("mkdir -p versions")
+        c.local("tar -czvf versions/{} web_static".format(archive_name))
+        return "versions/{}".format(archive_name)
+    except CommandFailed as e:
+        print(f"Failed to create the archive: {e}")
         return None
