8b07929aa0eae1218ab2830b1a85491aba4a7d2e
Wughanga Rose <wughangar.rose@gmail.com>
2023-09-09 00:39:31 -0700

    test file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-import os.path
-from fabric.api import local
-from datetime import datetime
+# bash script
 
+from fabric.api import local
+import os.path
+from datetime import datetime
 
 def do_pack():
     """
-    Generates a .tgz archive from the contents of the web_static folder.
+    function to generate .tgx from webstatic content directory
     """
-    # Get the current timestamp for the archive name
     now = datetime.utcnow()
-
     archive_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(now.year,
                                                                  now.month,
                                                                  now.day,
                                                                  now.hour,
                                                                  now.minute,
                                                                  now.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(archive_name)).failed is True:
+    if not os.path.exists("versions"):
+        local("mkdir -p versions")
+
+    result = local("LC_ALL=C tar -cvJf {} web_static".format(archive_name))
+    if result.failed:
         return None
     return archive_name
