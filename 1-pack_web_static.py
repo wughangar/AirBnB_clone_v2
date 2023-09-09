1278033a17c5d989620d58668c921a25aadb0f8f
Wughanga Rose <wughangar.rose@gmail.com>
2023-09-09 00:57:01 -0700

    compress files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-import os.path
-from fabric.api import local
+import os
+from fabric.api import local, lcd
 from datetime import datetime
-
 
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
     """
+    # Get the absolute path to the script's directory
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Get the absolute path to the 'versions' directory
+    versions_dir = os.path.join(script_dir, 'versions')
+
+    # Get the absolute path to the 'web_static' directory
+    web_static_dir = os.path.join(script_dir, 'web_static')
+
     # Get the current timestamp for the archive name
     now = datetime.utcnow()
 
-    archive_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(now.year,
-                                                                 now.month,
-                                                                 now.day,
-                                                                 now.hour,
-                                                                 now.minute,
-                                                                 now.second)
+    archive_name = "{}/web_static_{}{}{}{}{}{}.tgz".format(versions_dir,
+                                                           now.year,
+                                                           now.month,
+                                                           now.day,
+                                                           now.hour,
+                                                           now.minute,
+                                                           now.second)
     
     # Create the "versions" directory if it doesn't exist
-    if not os.path.exists("versions"):
-        local("mkdir -p versions")
+    if not os.path.exists(versions_dir):
+        local("mkdir -p {}".format(versions_dir))
 
-    # Create the .tgz archive
-    result = local("tar -cvzf {} web_static".format(archive_name))
+    # Change to the "web_static" directory and create the .tgz archive
+    with lcd(web_static_dir):
+        result = local("tar -cvzf {} .".format(archive_name))
 
     if result.failed:
         return None
