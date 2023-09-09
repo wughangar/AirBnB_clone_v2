33dd2e76e55f8a2f3710f255176aa2de97804efa
wughangar <wughangar.rose@gmail.com>
2023-09-09 15:13:36 +0300

    second question

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os.path
+import os
 from fabric.api import local
 from datetime import datetime
 
@@ -8,18 +8,19 @@ def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
     """
+    # Create the 'versions' directory if it doesn't exist
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
     # Get the current timestamp for the archive name
     now = datetime.utcnow()
+    archive_name = f"versions/web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
 
-    archive_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(now.year,
-                                                                 now.month,
-                                                                 now.day,
-                                                                 now.hour,
-                                                                 now.minute,
-                                                                 now.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(archive_name)).failed is True:
+    # Use the tar command to create the .tgz archive
+    result = local(f"tar -cvzf {archive_name} web_static")
+
+    # Check if the tar command was successful
+    if result.succeeded:
+        return archive_name
+    else:
         return None
-    return archive_name
