72b766ab564895e1d9481b101153e4802e5f123b
wughangar <wughangar.rose@gmail.com>
2023-09-10 08:25:32 +0300

    style

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,13 +8,13 @@ from datetime import datetime
 import os
 
 env.hosts = ['34.207.190.83', '52.91.178.39']
-env.user = '<ubuntu'
+env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_pack():
     """
-    fo pack function
+    do pack function
     """
     if not os.path.exists("versions"):
         os.makedirs("versions")
@@ -25,6 +25,7 @@ def do_pack():
         return archive_name
     else:
         return None
+
 
 def do_deploy(archive_path):
     """
@@ -37,21 +38,29 @@ def do_deploy(archive_path):
         archive_file = archive_path.split("/")[-1]
         archive_folder = archive_file.split(".")[0]
         run(f"mkdir -p /data/web_static/releases/{archive_folder}/")
-        run(f"tar -xzf /tmp/{archive_file} -C /data/web_static/releases/{archive_folder}/")
+        run(f"tar -xzf /tmp/{archive_file} -C /"
+            f"data/web_static/releases/{archive_folder}/")
         run(f"rm /tmp/{archive_file}")
-        run(f"mv /data/web_static/releases/{archive_folder}/web_static/* /data/web_static/releases/{archive_folder}/")
+        run(f"mv /data/web_static/releases/{archive_folder}/"
+            f"web_static/* /data/web_static/releases/{archive_folder}/")
         run(f"rm -rf /data/web_static/releases/{archive_folder}/web_static")
         run(f"rm -rf /data/web_static/current")
-        run(f"ln -s /data/web_static/releases/{archive_folder}/ /data/web_static/current")
+        run(f"ln -s /data/web_static/releases/"
+            f"{archive_folder}/ /data/web_static/current")
         return True
-    except:
+    except Exception:
         return False
+
 
 def deploy():
     """
-    do deploy function
+    deploy function
     """
     archive_path = do_pack()
     if not archive_path:
         return False
     return do_deploy(archive_path)
+
+
+if __name__ == "__main__":
+    deploy()
