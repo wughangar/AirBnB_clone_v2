03ace2fa83e0980ce2132afa7ae116ba35c0a4f5
wughangar <wughangar.rose@gmail.com>
2023-09-10 07:53:55 +0300

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,19 +4,37 @@ Fabric script to create and distribute an archive to web servers
 """
 
 import os
-from fabric.api import env, run
-from os.path import exists
-from fabric.operations import local, put
+from fabric.api import env, run, put, local
 from pack_web_static import do_pack
 from do_deploy_web_static import do_deploy
 
-env.hosts = ['34.207.190.83', '100.27.12.119']
+env.hosts = ['34.207.190.83', '52.91.178.39']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def create_new_version():
+    """
+    Create a new version of the archive locally with my_index.html inside
+    """
+    # Create a directory for the new version
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    # Copy 'my_index.html' into the new version directory
+    local("cp my_index.html versions/")
+
+    # Create the archive with the new version
+    archive_path = do_pack()
+
+    return archive_path
+
 
 def deploy():
     """
     Create and distribute an archive to web servers
     """
-    archive_path = do_pack()
+    archive_path = create_new_version()
     if archive_path is None:
         return False
     return do_deploy(archive_path)
