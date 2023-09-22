658b0158dcad6458bcbcf0bcccb652913362a93e
wughangar <wughangar.rose@gmail.com>
2023-09-22 05:43:05 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+0. Hello Flask!
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    """
+    function to display text
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    function to diapky hbnb
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    """
+    start the aplication on port 5000
+    """
+    app.run(host='0.0.0.0', port=5000)
