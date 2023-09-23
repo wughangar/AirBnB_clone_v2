6b2c94611dd9dad219bef57964eff6331ddd1e75
wughangar <wughangar.rose@gmail.com>
2023-09-23 03:22:03 +0000

    is it a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+4. Is it a number?
+"""
+
+import re
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    """
+    function to display text
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    function to diapky hbnb
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_display(text):
+    """
+    displays text after replacing _ with spaces
+    """
+    final_text = re.sub('_', ' ', text)
+    return f'C {final_text}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    displays python text
+    """
+    final_text = re.sub('_', ' ', text)
+    return f'Python {final_text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_int(n):
+    """
+    check if n is an int
+    """
+    if isinstance(n, int):
+        return f"{n} is a number"
+    else:
+        return "Not a valid integer"
+
+
+if __name__ == '__main__':
+    """
+    start the aplication on port 5000
+    """
+    app.run(host='0.0.0.0', port=5000)
